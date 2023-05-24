643fc04cfbbd96693c9b69c6177f0781467be042
SHOCK <beponuka666@gmail.com>
2023-05-24 18:14:23 +0500

    Task 9 compite

--- 9.py
+++ 9.py
@@ -3,3 +3,18 @@
 # while
 
 # 5 -> 120
+
+n = int(input("Введите n: "))
+count = 2
+sum = 1
+if n == 0:
+	print("0! = 1")
+elif n == 1:
+	print("1! = 1")
+elif n < 0:
+	print("Введите положительное число!")
+	
+while count <= n and n > 0:
+	sum = sum * count
+	count += 1
+print(n,"! = ",sum)
