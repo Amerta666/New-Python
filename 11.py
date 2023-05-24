3fc130ac802961deb4ef1301f85e4dbbffc345bb
SHOCK <beponuka666@gmail.com>
2023-05-24 18:19:57 +0500

    Task 11 compite

--- 11.py
+++ 11.py
@@ -3,3 +3,22 @@
 
 # 5 -> 6
 
+a = int(input("Введите A: "))
+count = 3
+sum = 1
+sum1 = 1
+sum2 = 1
+
+if a == 0 or a == 1 or a < 0:
+	print("Число А должно быть больше 1!")
+	exit()
+
+while sum != a and count < 20:
+	sum1 = sum2
+	sum2 = sum
+	sum = sum2 + sum1
+	count +=1
+	0
+if sum == a:
+	print("Число", a, "является", count, "по счёту Фибоначчи")
+else: print("-1")
