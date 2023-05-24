e79266c0325fedcac9b73ec0dc926f30d4b42905
SHOCK <beponuka666@gmail.com>
2023-05-24 20:33:48 +0500

    Improved code in task  9

--- 9.py
+++ 9.py
@@ -4,17 +4,27 @@
 
 # 5 -> 120
 
-n = int(input("Введите n: "))
 count = 2
 sum = 1
+a = True
+
+while a:
+	try:
+		n = int(input("Введите n: "))
+		if n >= 0:
+			a = False
+		else: print("Введите положительное целое число!")
+	except:
+		print("Введите положительное целое число!")
+
 if n == 0:
-	print("0! = 1")
+	print("0 ! = 1")
+	exit()
 elif n == 1:
-	print("1! = 1")
-elif n < 0:
-	print("Введите положительное число!")
-	
-while count <= n and n > 0:
+	print("1 ! = 1")
+	exit()
+
+while n >= count and n > 1:
 	sum = sum * count
 	count += 1
 print(n,"! = ",sum)
