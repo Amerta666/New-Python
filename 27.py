2ffd556fc2e33edd38fb40a2d12ec772de6bbcc7
SHOCK <beponuka666@gmail.com>
2023-05-31 20:45:45 +0500

    Add and complete task 27

---
+++ 27.py
@@ -0,0 +1,19 @@
+# Пользователь вводит текст(строка). Словом считается последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+#
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+#
+# Output: 13
+
+text = "She sells sea shells on the sea shore The shells that she sells are sea shells " \
+       "I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells " \
+       "are sea shore shells"
+
+text_2 = text.strip(".,!?\n").lower().split()
+print(f"В данном отрывке {len(text_2)} слов из них {len(set(text_2))} различных.")
+
+
