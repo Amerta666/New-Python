ddc62dc5e9b7243a1f3149caea7ef782519712ef
SHOCK <beponuka666@gmail.com>
2023-05-31 20:11:09 +0500

    Task 11 change code

--- 11 func.py
+++ 11 func.py
@@ -1,5 +1,6 @@
 b = True
 
+
 def check_fib_pos(A):
     count = 3
     fib_prev = 1
@@ -9,20 +10,23 @@ def check_fib_pos(A):
         fib_prev = fib_curr
         fib_curr = fib_next
         fib_next = fib_curr + fib_prev
-        count +=1
-    
+        count += 1
+
     if fib_next == A:
         print("Число", A, "является", count, "-ым числом Фибоначчи")
-    else: print("Число", A, "не является числом Фибоначчи и равно -1")
+    else:
+        print("Число", A, "не является числом Фибоначчи и равно -1")
     return count
+
 
 while b:
     try:
-		A = int(input("Введите A: "))
-		if A > 1:
-			b = False
-		else: print("Число А должно быть больше 1!")
-	except:
-		print("Введите число!")
+        A = int(input("Введите A: "))
+        if A > 1:
+            b = False
+        else:
+            print("Число А должно быть больше 1!")
+    except:
+        print("Введите число!")
 
 print(check_fib_pos(A))
