5b98931b8e4cc89ebb952cc345ddbffe13fdc7e8
SHOCK <beponuka666@gmail.com>
2023-06-09 22:02:31 +0500

    Task 33 complete

--- 33.py
+++ 33.py
@@ -4,3 +4,24 @@
 #
 # Input: 5 -> 1 3 3 3 4
 # Output: 1 3 3 3 1
+
+list_1 = [2, 4, 5, 3, 1, 5]
+print(list_1)
+def replace(n):
+    max_num = n[0]
+    min_num = n[0]
+    for i in range(len(n)):
+        if n[i] > max_num:
+            max_num = n[i]
+        if n[i] < min_num:
+            min_num = n[i]
+    if max_num == min_num:
+        return print("Все оценки одинаковые")
+    for i in range(len(n)):
+        if n[i] == max_num:
+            n[i] = min_num
+    print(f'Минимальная оценка - {min_num}')
+    print(f'Максимальная оценка - {max_num}')
+    return n
+
+print(replace(list_1))
