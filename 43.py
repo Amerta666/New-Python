2613618ad22a00e54401cf6af0c88aec8d8b002f
SHOCK <beponuka666@gmail.com>
2023-06-10 22:19:03 +0500

    Task 43 complete

--- 43.py
+++ 43.py
@@ -5,3 +5,23 @@
 #
 # Ввод:         Вывод:
 # 1 2 3 2 3     2
+import random
+
+list_1 = list(random.randint(0, 10) for i in range(random.randint(5, 11)))
+print(list_1)
+
+list_2 = []
+
+count = 0
+for i in range(len(list_1)-1):
+    j = i + 1
+    while j < len(list_1):
+        if list_1[i] == list_1[j]:
+            count += 1
+        j += 1
+
+print(count)
+
+
+
+
