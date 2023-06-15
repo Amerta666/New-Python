e8e6a2d4f2c44c9fe0c0f65f43a056fc0d2aa28a
SHOCK <beponuka666@gmail.com>
2023-06-15 22:28:49 +0500

    Improved code in task 49

--- 49.py
+++ 49.py
@@ -34,4 +34,4 @@ def find_farthest_orbit(list_of_orbits):
 orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
 
 print(find_farthest_orbit(orbits))
-
+print(max(orbits, key=lambda x: (x[0] != x[1]) * x[0] * x[1]))
