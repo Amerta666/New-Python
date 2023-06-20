4b8d942f1f66ab705ea37c31329c23702868408f
SHOCK <beponuka666@gmail.com>
2023-06-20 20:39:11 +0500

    Alpha Release "My phone book"

--- Home work/Guide/main.py
+++ Home work/Guide/main.py
@@ -5,7 +5,7 @@ data_file = 'contacts.txt'
 def print_commands():
     return print('Введите интересующую вас команду:'
                  '\n 1 - Показать все записи\n 2 - Поиск\n 3 - Добавить контакт\n 4 - Удалить контакт'
-                 '\n 0 - Выход')
+                 '\n 5 - Редактировать контакт\n 0 - Выход')
 
 def read_data(file):
     with open(file, 'r', encoding="utf-8") as data:
@@ -50,9 +50,9 @@ def add_record():
             max_num = int(i[0])
     print('Добавьте данные')
     number = str(max_num + 1)
-    surname = input('Введите фамилию: ')
-    name_first = input('Введите имя: ')
-    name_second = input('Введите отчество: ')
+    surname = input('Введите фамилию: ').capitalize()
+    name_first = input('Введите имя: ').capitalize()
+    name_second = input('Введите отчество: ').capitalize()
     phonenumber = input('Введите номер телефона: ')
     n = '\n'
     phone_book.append(str(number) + ',' + str(surname) + ',' + str(name_first) + ',' + str(name_second) + ',' + str(phonenumber))
@@ -61,15 +61,100 @@ def add_record():
 def del_record():
     del_num = input('Введите идентефикатор удаляемой записи: ')
     phone_book = read_data(data_file)
-    confirm = input('Введите "Y" для подтверждения или "N" для отмены: ')
-    if confirm == 'Y':
-        for line in phone_book:
-            if line[0] == del_num:
+    for line in phone_book:
+        if line[0] == del_num:
+            print(line)
+            confirm = input('Введите "Y" для подтверждения или "N" для отмены: ')
+            if confirm == 'Y':
                 phone_book.pop(phone_book.index(line))
                 write_data(data_file, phone_book)
                 print('Контакт удалён')
                 return
+            return
     return print('Контакт не найден')
+
+def edit_record():
+    edit = input('Введите идентефикатор редактируемой записи: ')
+    phone_book = read_data(data_file)
+    for line in phone_book:
+        if line[0] == edit:
+            print(line)
+            confirm = input('Введите "Y" для подтверждения или "N" для отмены: ')
+            if confirm == 'Y':
+                edit_num = int(input('Какую запись запись желаете отредактировать?\n'
+                      '1 - Фамилию, 2 - Имя, 3 - Отчество, 4 - Номер телефона: '))
+                if edit_num == 1:
+                    new_surname = input('Введите фамилию: ').capitalize()
+                    new_name_first = parse(2, line)
+                    new_name_second = parse(3, line)
+                    new_phonenumber = parse(4, line)
+                elif edit_num == 2:
+                    new_surname = parse(1, line)
+                    new_name_first = input('Введите имя: ').capitalize()
+                    new_name_second = parse(3, line)
+                    new_phonenumber = parse(4, line)
+                elif edit_num == 3:
+                    new_surname = parse(1, line)
+                    new_name_first = parse(2, line)
+                    new_name_second = input('Введите отчество: ').capitalize()
+                    new_phonenumber = parse(4, line)
+                elif edit_num == 4:
+                    new_surname = parse(1, line)
+                    new_name_first = parse(2, line)
+                    new_name_second = parse(3, line)
+                    new_phonenumber = input('Введите номер телефона: ')
+                else:
+                    return print('Некоректный код команды')
+
+                phone_book[phone_book.index(line)] = (str(edit) + ',' + str(new_surname)
+                                                      + ',' + str(new_name_first) + ',' + str(new_name_second)
+                                                      + ',' + str(new_phonenumber))
+                write_data(data_file, phone_book)
+                return
+    return print('Контакт не найден')
+
+
+
+def parse(number_name, line):
+    phone_book = read_data(data_file)
+    count = 0
+    result = ''
+    for word in line:
+        if word == ' ':
+            count += 1
+        if word != ' ':
+            if count == number_name:
+                result += word
+    return result
+
+def parse_all(number, phone_book):
+    surname = ''
+    first_name = ''
+    second_name = ''
+    tel_nember = ''
+    count = 0
+    for line in phone_book:
+        if line[0] == number:
+            for word in line:
+                print(word)
+                if word == ' ':
+                    count += 1
+                if word != ' ':
+                    if count == 1:
+                        surname += word
+                    if count == 2:
+                        first_name += word
+                    if count == 3:
+                        second_name += word
+                    if count == 4:
+                        tel_nember += word
+        return list1.append(number, surname, first_name, second_name, tel_nember)
+
+def test():
+    number = '5'
+    phone_book = read_data(data_file)
+
+    return print(list1)
 
 
 clear = lambda: os.system('cls')
@@ -87,7 +172,11 @@ while True:
         add_record()
     elif enter_num == 4:
         del_record()
+    elif enter_num == 5:
+        edit_record()
+    elif enter_num == 6:
+        test()
     elif enter_num == 0:
         exit()
     else:
-        print('ELSE')
+        print('Неверный код команды')
