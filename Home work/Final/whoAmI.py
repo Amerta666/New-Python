c0a55db972d6c22eb81a38c300ec6024d4aab374
SHOCK <beponuka666@gmail.com>
2023-06-23 01:02:56 +0500

    Add and complete task 'Final'

--- Home work/Final/whoAmI.py
+++ Home work/Final/whoAmI.py
@@ -7,7 +7,7 @@
 # lst = ['robot'] * 10
 # lst += ['human'] * 10
 # random.shuffle(lst)
-# data = pd.DataFrame({'whoAmI'lst})
+# data = pd.DataFrame({'whoAmI:lst})
 # data.head() |
 
 import pandas as pd
